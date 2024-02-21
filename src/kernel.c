298edb190421f13ab65edaee8d9be57e0dfa50fe
zzuse <zzuse@outlook.com>
2024-02-20 23:51:45 -0600

    enable paging

--- src/kernel.c
+++ src/kernel.c
@@ -4,6 +4,7 @@
 #include "idt/idt.h"
 #include "io/io.h"
 #include "memory/heap/kheap.h"
+#include "memory/paging/paging.h"
 
 uint16_t *video_mem = 0;
 uint16_t terminal_row = 0;
@@ -68,7 +69,7 @@ void print(const char *str)
 }
 
 // extern void problem();
-
+static struct paging_4gb_chunk *kernel_chunk = 0;
 void kernel_main()
 {
     terminal_initialize();
@@ -78,6 +79,11 @@ void kernel_main()
     kheap_init();
     // Initialize the interrupt descriptor table
     idt_init();
+
+    kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
+    paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
+    // Enable Paging
+    enable_paging();
     // Enable the system interrupts
     enable_interrupts();
     // 1. problem();
