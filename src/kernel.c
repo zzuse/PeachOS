ef9c19d3134025fe769753a9ed0d8a1742e1c493
zzuse <zzuse@outlook.com>
2024-02-27 22:51:59 -0600

    file

--- src/kernel.c
+++ src/kernel.c
@@ -5,7 +5,9 @@
 #include "io/io.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "string/string.h"
 #include "disk/disk.h"
+#include "fs/pparser.h"
 
 uint16_t *video_mem = 0;
 uint16_t terminal_row = 0;
@@ -50,16 +52,6 @@ void terminal_initialize()
     }
 }
 
-size_t strlen(const char *str)
-{
-    size_t len = 0;
-    while (str[len])
-    {
-        len++;
-    }
-    return len;
-}
-
 void print(const char *str)
 {
     size_t len = strlen(str);
@@ -88,15 +80,17 @@ void kernel_main()
     kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
     paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
 
-    char *ptr = kzalloc(4096);
-    paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void *)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITEABLE);
+    // 6.
+    // char *ptr = kzalloc(4096);
+    // paging_set(paging_4gb_chunk_get_directory(kernel_chunk), (void *)0x1000, (uint32_t)ptr | PAGING_ACCESS_FROM_ALL | PAGING_IS_PRESENT | PAGING_IS_WRITEABLE);
 
     // Enable Paging
     enable_paging();
 
-    char buf[512];
-    disk_get(0);
-    disk_read_block(disk_get(0), 20, 4, &buf);
+    // 7.
+    // char buf[512];
+    // disk_get(0);
+    // disk_read_block(disk_get(0), 20, 4, &buf);
 
     // 5.
     // char buf[512];
@@ -114,7 +108,7 @@ void kernel_main()
 
     // 2. outb(0x60, 0xff);
 
-    /* 3.
+    // 3.
     // void *ptr = kmalloc(50);
     // void *ptr2 = kmalloc(5000);
     // void *ptr3 = kmalloc(5600);
@@ -122,6 +116,10 @@ void kernel_main()
     // void *ptr4 = kmalloc(50);
     // if (ptr || ptr2 || ptr3 || ptr4)
     // {
-    //}
-    */
+    // }
+    struct path_root *root_path = pathparser_parse("0:/bin/shell.exe", NULL);
+
+    if (root_path)
+    {
+    }
 }
