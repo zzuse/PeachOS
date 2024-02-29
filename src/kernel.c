c5e9c4dbec1cb2c7f6fe6a6ed394962cbbcc280c
zzuse <zzuse@outlook.com>
2024-02-28 23:57:29 -0600

    disk stream

--- src/kernel.c
+++ src/kernel.c
@@ -8,6 +8,7 @@
 #include "string/string.h"
 #include "disk/disk.h"
 #include "fs/pparser.h"
+#include "disk/streamer.h"
 
 uint16_t *video_mem = 0;
 uint16_t terminal_row = 0;
@@ -117,9 +118,18 @@ void kernel_main()
     // if (ptr || ptr2 || ptr3 || ptr4)
     // {
     // }
-    struct path_root *root_path = pathparser_parse("0:/bin/shell.exe", NULL);
 
-    if (root_path)
+    // 8.
+    // struct path_root *root_path = pathparser_parse("0:/bin/shell.exe", NULL);
+    // if (root_path)
+    // {
+    // }
+
+    struct disk_stream *stream = diskstreamer_new(0);
+    diskstreamer_seek(stream, 0x201);
+    unsigned char c = 0;
+    diskstreamer_read(stream, &c, 1);
+    while (1)
     {
     }
 }
